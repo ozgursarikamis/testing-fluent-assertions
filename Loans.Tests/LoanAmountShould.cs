5d9e84cc6ac986813cd5e3698fb31d2cee471de7
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-15 02:33:45 +0300

    projects setup

---
+++ Loans.Tests/LoanAmountShould.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Loans.Domain.Applications.Values;
+using NUnit.Framework;
+
+namespace Loans.Tests
+{
+    public class LoanAmountShould
+    {
+        [Test]
+        public void StoreCurrencyCode()
+        {
+            var loanAmount = new LoanAmount("USD", 100_000);
+            loanAmount.CurrencyCode.Should().Be("USD");
+            //Assert.That(sut.CurrencyCode, Is.EqualTo("USD"));
+        }
+    }
+}
